401327449a14519d6a01b1d22533896a17de8311
Kyle <kjfossman@gmail.com>
2021-04-15 13:28:51 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :tags, through: :post_tags
+  has_many :tags, through: :users
   validates :name, presence: true
   validates :content, presence: true
 end
